f771f06143b387f30b1366a1fc043d8232c12ca7
Aswathy PA <aswathylkh67@gmail.com>
2025-11-07 10:55:12 +0530

    cw1

---
+++ day1cw.py
@@ -0,0 +1,13 @@
+apple = 15.5
+orange = 20
+grape = 10.25
+total_volume = apple + orange + grape
+print("Total volume of juice:", total_volume)
+total_int = int(total_volume)
+print("Total volume in integer:", total_int)
+total_str = str(total_volume)
+print("Total volume as string:", total_str)
+import random
+random_number = random.randrange(5,10)
+final_total = total_volume + random_number
+print("Final total after adding random number:", final_total)
